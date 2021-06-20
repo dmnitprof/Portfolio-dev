3cc6ed42e2583340b5ba69edccf75ef32a8790e0
dmnitprof <dmn.itprof@gmail.com>
2021-06-21 02:32:45 +0300

    project Portfolio-dev - edit 2

--- scripts/script.js
+++ scripts/script.js
@@ -6,17 +6,19 @@ function Onload() {
         pageLoading.style.display = 'none'
     }, 500)
 }
-
+const page = document.querySelector('.page')
 const burger = document.querySelector('.burger__span')
 const menu = document.querySelector('.header-nav__menu')
 
-const switchClass = ['active', 'show-menu']
+const switchClass = ['active', 'show-menu', 'hidden']
+
+function animateElement(element, index) {
+    element.classList.toggle(switchClass[index])
+}
 
 burger.addEventListener('click', (e) => {
         animateElement(burger, 0)
         animateElement(menu, 1)
+    animateElement(page, 2)
 })
 
-function animateElement(element, index) {
-   element.classList.toggle(switchClass[index])
-}
