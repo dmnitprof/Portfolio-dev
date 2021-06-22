7aa29b36e560e09082cdd6891b3433a7deb29886
dmnitprof <54173914+dmnitprof@users.noreply.github.com>
2021-06-22 09:11:15 +0300

    edit app.js

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-import './styles/Base.scss'
-import './scripts/script.js'
+//import './styles/Base.scss'
+//import './scripts/script.js'
+
+document.addEventListener('DOMContentLoaded', Onload)
+
+function Onload() {
+    const pageLoading = document.querySelector('.page .loading')
+    setTimeout(() => {
+        pageLoading.style.display = 'none'
+    }, 500)
+}
+const page = document.querySelector('.page')
+const burger = document.querySelector('.burger__span')
+const menu = document.querySelector('.header-nav__menu')
+
+const switchClass = ['active', 'show-menu', 'hidden']
+
+function animateElement(element, index) {
+    element.classList.toggle(switchClass[index])
+}
+
+burger.addEventListener('click', (e) => {
+        animateElement(burger, 0)
+        animateElement(menu, 1)
+    animateElement(page, 2)
+})
+
